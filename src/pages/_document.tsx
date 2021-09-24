aaf687b10906dc0c1f8d231c087d1cbd496aa316
Karleon Cristophe <karleoncristophe@hotmail.com>
2021-09-24 17:46:42 -0300

    Altered imag and Time

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,27 @@
-import Document, {Html, Head, Main, NextScript} from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
-    render(){
-        return (
-            <Html>
-                <Head>
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="icon"
+            href="https://cdn-icons-png.flaticon.com/512/2983/2983428.png"
+          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 // aqui fica tudo que é estatico
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
-import { GetServerSideProps } from 'next'
+import { GetServerSideProps } from "next";
 
 import { CompleteChallenges } from "../components/CompleteChallenges";
 import { CountDawn } from "../components/CountDawn";
-import { ExperienceBar } from "../components/ExperienceBar"; 
+import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { ChallengeBox } from "../components/ChallengeBox";
 
-import Head from 'next/head'
-import styles from '../styles/pages/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/pages/Home.module.css";
 import { CountdownProvider } from "../components/contexts/CountdownContext";
-import { ChallengesProvider } from '../components/contexts/ChallengesContexts';
+import { ChallengesProvider } from "../components/contexts/ChallengesContexts";
 
 interface HomeProps {
   level: number;
@@ -19,44 +19,39 @@ interface HomeProps {
 
 export default function Home(props: HomeProps) {
   return (
-    <ChallengesProvider 
-    level={props.level}
-     currentExperience={props.currentExperience} 
-     challengeCompleted={props.challengeCompleted} >
-
-    <div className={styles.container}>
-      <Head>
-        <title>Inicio | move.it</title>
-      </Head>
-      <ExperienceBar />
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompleteChallenges />
-            <CountDawn />
-          </div>
-          <ChallengeBox />
-          <div>
-
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
-
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengeCompleted={props.challengeCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Move It</title>
+        </Head>
+        <ExperienceBar />
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompleteChallenges />
+              <CountDawn />
+            </div>
+            <ChallengeBox />
+            <div></div>
+          </section>
+        </CountdownProvider>
+      </div>
     </ChallengesProvider>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
- 
-
-  const {level, currentExperience, challengeCompleted } = ctx.req.cookies
+  const { level, currentExperience, challengeCompleted } = ctx.req.cookies;
   return {
-    props: { 
+    props: {
       level: Number(level),
       currentExperience: Number(currentExperience),
       challengeCompleted: Number(challengeCompleted),
-    }
-  }
-}
+    },
+  };
+};
